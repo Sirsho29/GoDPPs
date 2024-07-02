d3b3d5867ad4f6450e3d63ebc9241903d32306af
sirsho29 <sirsha.cse@gmail.com>
2024-07-02 23:32:44 +0530

    Day03 Complete

--- Day03/Problem2/solution.go
+++ Day03/Problem2/solution.go
@@ -1,6 +1,19 @@
 package problem2
 
-func Solution() {
-    // Your solution here
-}
+import "fmt"
 
+func Solution() {
+
+	fmt.Println("Enter a non-negative integer:")
+
+	var num int
+	fmt.Scanf("%d", &num)
+	sum := 0
+
+	for num > 0 {
+		sum += num % 10
+		num /= 10
+	}
+
+	fmt.Println("Sum of digits:", sum)
+}
--- Day03/Problem3/solution.go
+++ Day03/Problem3/solution.go
@@ -1,6 +1,24 @@
 package problem3
 
-func Solution() {
-    // Your solution here
-}
+import (
+	"fmt"
+	"strings"
+)
 
+func Solution() {
+	var str string
+	fmt.Println("Enter a string:")
+	fmt.Scanln(&str)
+	count := 0
+	for i := 0; i < len(str); i++ {
+		if strings.ToLower(string(str[i])) == "a" || strings.ToLower(string(str[i])) == "e" || strings.ToLower(string(str[i])) == "i" || strings.ToLower(string(str[i])) == "o" || strings.ToLower(string(str[i])) == "u" {
+			count++
+		}
+	}
+
+	if count == 0 {
+		fmt.Println("No vowels found.")
+	} else {
+		fmt.Println("Number of vowels:", count)
+	}
+}
